44048bb6d68f169a49751f29d1da9148e0af726f
Juancho <linanjm90@gmail.com>
2019-08-25 00:29:54 -0500

    Add the initialization of dagger in the application class

--- app/src/main/java/mx/nakva/apphack/MyApp.kt
+++ app/src/main/java/mx/nakva/apphack/MyApp.kt
@@ -1,6 +1,9 @@
 package mx.nakva.apphack
 
 import android.app.Application
+import mx.nakva.apphack.di.AppComponent
+import mx.nakva.apphack.di.AppModule
+import mx.nakva.apphack.di.DaggerAppComponent
 
 /**
  * Created by Juancho - j.herandez@arteko.mx on 25/08/19.
@@ -8,7 +11,14 @@ import android.app.Application
  */
 class MyApp: Application() {
 
+    private lateinit var appComponent: AppComponent
     override fun onCreate() {
         super.onCreate()
+        appComponent = initDagger(this)
     }
+
+    private fun initDagger(app: MyApp): AppComponent =
+        DaggerAppComponent.builder()
+            .appModule(AppModule(app))
+            .build()
 }
