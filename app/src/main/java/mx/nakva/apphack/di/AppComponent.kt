3ffcfcc4b40fb507e46a3c7f401a47b423e931fb
Juancho <linanjm90@gmail.com>
2019-08-25 00:27:12 -0500

    Add the DI app component to inject the dependencies in the activities or fragments

---
+++ app/src/main/java/mx/nakva/apphack/di/AppComponent.kt
@@ -0,0 +1,14 @@
+package mx.nakva.apphack.di
+
+import dagger.Component
+import javax.inject.Singleton
+
+/**
+ * Created by Juancho - j.herandez@arteko.mx on 25/08/19.
+ * Powered by Arteko
+ */
+@Singleton
+@Component(modules = [AppModule::class])
+interface AppComponent {
+
+}
