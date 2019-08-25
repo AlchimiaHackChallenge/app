7c6b3fba478d8f3431e4f7495cff1f795529fe3a
Juancho <linanjm90@gmail.com>
2019-08-25 00:55:02 -0500

    add a new view model for the main feature

---
+++ app/src/main/java/mx/nakva/apphack/MainViewModel.kt
@@ -0,0 +1,11 @@
+package mx.nakva.apphack
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+/**
+ * Created by Juancho - j.herandez@arteko.mx on 25/08/19.
+ * Powered by Arteko
+ */
+class MainViewModel @Inject constructor(): ViewModel() {
+}
