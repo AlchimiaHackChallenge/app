55070423578b4ae71c43cedbf36b211142ed5208
Juancho <linanjm90@gmail.com>
2019-08-25 09:20:51 -0500

    create the view model for the profile feature

---
+++ app/src/main/java/mx/nakva/apphack/features/profile/ProfileViewModel.kt
@@ -0,0 +1,11 @@
+package mx.nakva.apphack.features.profile
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+/**
+ * Created by Juancho - j.herandez@arteko.mx on 25/08/19.
+ * Powered by Arteko
+ */
+class ProfileViewModel @Inject constructor(): ViewModel() {
+}
