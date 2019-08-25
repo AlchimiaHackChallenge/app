38c4f8f0c3f2320b4452ca7ddd1d5861396c4a32
Juancho <linanjm90@gmail.com>
2019-08-25 09:21:51 -0500

    add the view model provider for the profile feature

--- app/src/main/java/mx/nakva/apphack/di/ViewModelModule.kt
+++ app/src/main/java/mx/nakva/apphack/di/ViewModelModule.kt
@@ -6,6 +6,7 @@ import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 import mx.nakva.apphack.MainViewModel
+import mx.nakva.apphack.features.profile.ProfileViewModel
 import mx.nakva.apphack.features.survey.SurveyViewModel
 import javax.inject.Singleton
 
@@ -24,6 +25,12 @@ abstract class ViewModelModule {
     @ViewModelKey(MainViewModel::class)
     @Singleton
     internal abstract fun provideViewModel(viewModel: MainViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ProfileViewModel::class)
+    @Singleton
+    internal abstract fun provideProfileViewModel(viewModel: ProfileViewModel): ViewModel
 
     @Binds
     @IntoMap
