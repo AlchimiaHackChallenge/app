a3f95b86955b523ab672f43dbe370cf1f56d56a9
Juancho <linanjm90@gmail.com>
2019-08-25 02:37:53 -0500

    New View model in the survey feature

---
+++ app/src/main/java/mx/nakva/apphack/features/SurveyViewModel.kt
@@ -0,0 +1,11 @@
+package mx.nakva.apphack.features
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+/**
+ * Created by Juancho - j.herandez@arteko.mx on 25/08/19.
+ * Powered by Arteko
+ */
+class SurveyViewModel @Inject constructor(): ViewModel() {
+}
