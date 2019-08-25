9b8e22e075b364b2bcecff3336e0a5a940184234
Juancho <linanjm90@gmail.com>
2019-08-25 02:37:19 -0500

    add a new observable to launch the survey activity when the user click the new session btn

--- app/src/main/java/mx/nakva/apphack/MainActivity.kt
+++ app/src/main/java/mx/nakva/apphack/MainActivity.kt
@@ -3,10 +3,14 @@ package mx.nakva.apphack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.Observable
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 import mx.nakva.apphack.databinding.ActivityMainBinding
+import mx.nakva.apphack.features.SurveyActivity
+import mx.nakva.apphack.wrappers.Event
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -23,7 +27,15 @@ class MainActivity : AppCompatActivity() {
     private fun initDI() {
         (application as  MyApp).appComponent.inject(this)
         val vm = ViewModelProviders.of(this, viewModelFactory)[MainViewModel::class.java]
+        vm.getSurveyObserver().observe(this, Observer { onSurveyEventChange(it) })
         bindLayout(vm)
+    }
+
+    private fun onSurveyEventChange(event: Event<Int>?) {
+        event?.getContentIfNotHandled()?.let { requestCode ->
+            val intent = SurveyActivity.getIntent(this)
+            startActivityForResult(intent, requestCode)
+        }
     }
 
     private fun bindLayout(vm: MainViewModel) {
--- app/src/main/java/mx/nakva/apphack/MainViewModel.kt
+++ app/src/main/java/mx/nakva/apphack/MainViewModel.kt
@@ -2,7 +2,10 @@ package mx.nakva.apphack
 
 import android.util.Log
 import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import mx.nakva.apphack.wrappers.Event
 import javax.inject.Inject
 
 /**
@@ -12,8 +15,11 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(): ViewModel() {
 
     private val mState = MainState()
+    private val mSurveyLauncher = MutableLiveData<Event<Int>>()
 
     fun getState(): MainState = mState
+
+    fun getSurveyObserver(): LiveData<Event<Int>> = mSurveyLauncher
 
     fun onClickSearchBtn() {
         Log.d(TAG, "NAILAH onClickSearchBtn: ")
@@ -21,7 +27,7 @@ class MainViewModel @Inject constructor(): ViewModel() {
     }
 
     fun onClickNewSession() {
-        Log.d(TAG, "NAILAH onClickNewSession: ")
+        mSurveyLauncher.value = Event(1)
     }
     
     companion object {
