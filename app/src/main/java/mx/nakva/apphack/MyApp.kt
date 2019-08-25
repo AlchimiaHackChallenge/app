51ea02440b5861c2305e8dda791956708ab0dd22
Juancho <linanjm90@gmail.com>
2019-08-25 00:19:42 -0500

    new application class file

---
+++ app/src/main/java/mx/nakva/apphack/MyApp.kt
@@ -0,0 +1,14 @@
+package mx.nakva.apphack
+
+import android.app.Application
+
+/**
+ * Created by Juancho - j.herandez@arteko.mx on 25/08/19.
+ * Powered by Arteko
+ */
+class MyApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
