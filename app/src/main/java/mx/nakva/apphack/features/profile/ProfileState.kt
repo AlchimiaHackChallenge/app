9affef52deeb175a07774b051ba31b6e9009c658
Juancho <linanjm90@gmail.com>
2019-08-25 09:25:32 -0500

    add the state class in the profile feature and the layout bindable tags in the layoyt

---
+++ app/src/main/java/mx/nakva/apphack/features/profile/ProfileState.kt
@@ -0,0 +1,10 @@
+package mx.nakva.apphack.features.profile
+
+import androidx.databinding.BaseObservable
+
+/**
+ * Created by Juancho - j.herandez@arteko.mx on 25/08/19.
+ * Powered by Arteko
+ */
+class ProfileState: BaseObservable() {
+}
